d14369fc8185f7693de648610101b5585b79ef29
yutanaka <gamasann21@yahoo.co.jp>
2022-10-05 23:36:57 +0900

    wip

--- test/models/micropost_test.rb
+++ test/models/micropost_test.rb
@@ -4,8 +4,7 @@ class MicropostTest < ActiveSupport::TestCase
 
   def setup
     @user = users(:michael)
-    # このコードは慣習的に正しくない
-    @micropost = Micropost.new(content: "Lorem ipsum", user_id: @user.id)
+    @micropost = @user.microposts.build(content: "Lorem ipsum")
   end
 
   test "should be valid" do
@@ -15,5 +14,9 @@ class MicropostTest < ActiveSupport::TestCase
   test "user id should be present" do
     @micropost.user_id = nil
     assert_not @micropost.valid?
+  end
+
+  test "order should be most recent first" do
+    assert_equal microposts(:most_recent), Micropost.first
   end
 end
