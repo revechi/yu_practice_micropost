9c9611322ea1930b3fa9bdf56cb5afe5ed0c8eca
yutanaka <gamasann21@yahoo.co.jp>
2022-10-06 00:42:26 +0900

    wip

--- app/models/user.rb
+++ app/models/user.rb
@@ -58,6 +58,12 @@ class User < ApplicationRecord
     UserMailer.account_activation(self).deliver_now
   end
 
+  # 試作feedの定義
+  # 完全な実装は次章の「ユーザーをフォローする」を参照
+  def feed
+    Micropost.where("user_id = ?", id)
+  end
+
   private
   
     # メールアドレスを全て小文字にする
