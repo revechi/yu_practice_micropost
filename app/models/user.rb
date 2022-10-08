23d474824277044c17d618556a5633d44b99eb62
yutanaka <gamasann21@yahoo.co.jp>
2022-10-08 13:38:36 +0900

    14章

--- app/models/user.rb
+++ app/models/user.rb
@@ -74,6 +74,19 @@ class User < ApplicationRecord
     Micropost.where("user_id = ?", id)
   end
 
+  # パスワード再設定の期限が切れている場合はtrueを返す
+  def password_reset_expired?
+    reset_sent_at < 2.hours.ago
+  end
+
+  # ユーザーのステータスフィードを返す
+  def feed
+    following_ids = "SELECT followed_id FROM relationships
+                      WHERE follower_id = :user_id"
+    Micropost.where("user_id IN (#{following_ids})
+                     OR user_id = :user_id", user_id: id)
+  end
+
   # ユーザーをフォローする
   def follow(other_user)
     following << other_user
